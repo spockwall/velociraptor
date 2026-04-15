7e09b523ccaee11e10174689c33aa10a520c0d34
spockwall <abcdefg35216874@gmail.com>
2026-04-15 18:02:09 +0800

    feat: remove old configs

--- orderbook/src/configs/logging.rs
+++ orderbook/src/configs/logging.rs
@@ -1,21 +1,4 @@
-use serde::Deserialize;
 use tracing_subscriber::EnvFilter;
-
-#[derive(Debug, Deserialize)]
-#[serde(default)]
-pub struct LoggingConfig {
-    pub level: String,
-    pub json: bool,
-}
-
-impl Default for LoggingConfig {
-    fn default() -> Self {
-        Self {
-            level: "info".into(),
-            json: false,
-        }
-    }
-}
 
 pub fn init_logging(filter: &str, json: bool) {
     let env_filter = EnvFilter::try_new(filter).unwrap_or_else(|_| EnvFilter::new("info"));
