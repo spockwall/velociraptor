3104ac6ea772b211e6e743e0c01495724e19fd56
spockwall <abcdefg35216874@gmail.com>
2026-04-16 19:07:14 +0800

    fix: use correct ticker resolver

--- orderbook/src/exchanges/kalshi/mod.rs
+++ orderbook/src/exchanges/kalshi/mod.rs
@@ -8,4 +8,7 @@ pub mod utils;
 pub use connection::KalshiConnection;
 pub(crate) use msg_parser::KalshiMessageParser;
 pub use subscription::KalshiSubMsgBuilder;
-pub use utils::{build_ticker, current_window_close, current_window_start, format_ticker_dt};
+pub use utils::{
+    build_event_ticker, current_window_close, current_window_start, format_ticker_dt,
+    resolve_market_ticker,
+};
